dd263d86b0649d1adcd7af9e9f9fa9ce3979fbae
minilabmemo <minilabmemo@gmail.com>
2024-01-31 10:27:46 +0800

    Add: MPA page for /meow/

---
+++ meow/MeowApp.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  123
+  <HelloWorld msg="page did it!" />
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -12,5 +12,13 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  }, 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        meow: resolve(__dirname, 'meow/index.html'),
+      },
+    },
+  },
 })
